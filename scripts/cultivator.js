90e1fc12d489601145fab7f48c9fc15f9b1de783
aprilkrgonzales <april.gonzales@generalassemb.ly>
2021-10-22 11:26:06 -0600

    created new files to keep functionality separate, stat buttons aren't working like they did last night

---
+++ scripts/cultivator.js
@@ -0,0 +1,33 @@
+console.log("cultivator.js loaded")
+
+class Cultivator {
+    // === ! Constructor ! === //
+    constructor(name) {
+        this.name = name;
+        // === ! Attributes ! === //
+        this.sect = "Yunmeng Jiang";
+        this.goldenCore = true;
+        this.trainingLevel = 0;
+        this.playLevel = 0;
+        this.eatLevel = 0;
+        this.sleepLevel = 0;
+        this.timer;
+    }
+    // === ! Methods ! === //
+    increaseTime() {
+        // console.log(this, 'this in increasetime');
+        this.timer = setInterval(function(){
+            console.log("tick")
+        }, 1000)
+    }
+    changeStatLevel(statToChange) {
+        console.log(statToChange, 'this is the stat')
+        weiYing.statToChange += 1;
+        // get stat id/name from a button click (passing in a string)
+        // loop through the keys on the cultivator object and if one matches the string THEN
+        // change the cultivator.stat_id value
+        // // have to pass in the cultivator and the string from the button click
+    }
+}
+const weiYing = new Cultivator("Wei Ying");
+// console.log(weiYing, 'wei ying');
---
+++ scripts/game.js
@@ -0,0 +1,24 @@
+console.log("game.js loaded")
+
+class Game {
+    // === ! Constructor ! === //
+    constructor() {
+
+        // === ! Attributes ! === //
+        this.gameOn = null;
+        this.timer = null;
+    }
+    // === ! Methods ! === //
+    gamePowerBtn = () => {
+        // === toggles the gameOn attribute on or off, returns Boolean === //
+        if(this.gameOn === null) {
+            this.gameOn = true;
+        } else {
+            this.gameOn = !this.gameOn;
+        }
+        return this.gameOn;
+    }
+}
+
+const game = new Game();
+// console.log(game);

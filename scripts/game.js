8f669a3af6a50d295f6851312d6fcc574cf0019f
aprilkrgonzales <april.gonzales@generalassemb.ly>
2021-10-25 12:39:56 -0600

    commit before attempting ghpages deploy

--- scripts/game.js
+++ scripts/game.js
@@ -18,43 +18,10 @@ class Game {
         }
         return this.gameOn;
     }
+    checkPlayerStats = () => {
+        
+    }
 }
 
 const game = new Game();
 // console.log(game);
-
-
-
-// console.log("play.js loaded")
-
-// // ===== START GAME BUTTON ===== //
-// const startBtn = document.getElementById("start__btn");
-// const gameStart = startBtn.addEventListener("click", function() {
-//     game.gamePowerBtn();
-//     console.log(game.gameOn, "game on status")
-//     console.log(game, 'is the timer running')
-//     game.timer = setInterval(function(){
-//         // game.timer += 1;
-//         // console.log(game.timer)
-//         console.log("click")
-//     }, 1000)
-// });
-
-// const stopBtn = document.getElementById("stop__btn");
-// stopBtn.addEventListener("click", function(){
-//     // console.log(game.timer, 'timer')
-//     clearInterval(game.timer)
-// })
-
-
-// // ===== STAT LEVEL BUTTONS ===== //
-// const buttons = document.getElementById("statBtn__board");
-// // // problem with adding event listener this way: a user could click outside of the button but inside the #statBtn__board section, which will give a string that won't be useful later // //
-// buttons.addEventListener("click", changeStat);
-// function changeStat(event) {
-//     console.log(weiYing)
-//     const statId = event.target.id + "Level";
-//     weiYing.changeStatLevel(statId)
-//     console.log(weiYing)
-// };
-

03d3a3da2087ab9643d2061eff1b13f114dbcdca
aprilkrgonzales <april.gonzales@generalassemb.ly>
2022-02-09 20:41:05 -0700

    start game function runs on page load.

--- scripts/play.js
+++ scripts/play.js
@@ -3,10 +3,6 @@ const startGameFunction = function () {
     // // ===== START GAME BUTTON ===== //
     Game.gameArr[0].gamePowerBtn();
     console.log("game on:", Game.gameArr[0].gameOn);
-	// === ! interval timers for gameplay ! === //
-	setInterval(function() {handleTimer()}, 1000);
-	setInterval(function() {handleStat()}, 1000);
-	setInterval(function() {stopGameFunction()}, 500);
 	// === ! remove start button ! === //
 	const startBtn = document.querySelector("#start");
 	startBtn.remove();
@@ -50,12 +46,19 @@ const handleTimer = function() {
 	// === ! set innerHTML ! === //
 	timer.innerHTML = Game.gameObj.timer;
 	Game.gameObj.timer++;
+	// return Game.gameObj.timer;
 };
 
 const stopGameFunction = function() {
+	// === ! only turn the game off if it's on ! === //
+	if (Game.gameObj.gameOn === false) return;
+	console.log(Game.gameObj.gameOn, 'game on? from stop game function')
 	// === ! check if relevant obj properties are greater than 1, ie. character is still alive ! === //
 	if (Cultivator.cultivatorObj.playLevel < 1 || Cultivator.cultivatorObj.eatLevel < 1 || Cultivator.cultivatorObj.sleepLevel < 1) {
 		console.log("stop game function invoked");
+		console.log(Game.gameObj.gameOn, '<= should be true')
+		Game.gameObj.gameOn = !Game.gameObj.gameOn;
+		console.log(Game.gameObj.gameOn, '<= should be false')
 	};
 };
 const render = function() {
@@ -80,4 +83,14 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < statBtns.length; i++) {
 		statBtns[i].addEventListener("click", clickStat);
     };
+	// === ! interval timers for gameplay ! === //
+	do {
+		setInterval(function() {handleTimer()}, 1000);
+		setInterval(function() {handleStat()}, 1000);
+		setInterval(function() {stopGameFunction()}, 500);
+	} while (Game.gameObj.gameOn === true)
+	if (Game.gameObj.gameOn === false) {
+		console.log("game over from the document listener");
+	}
+	
 });
